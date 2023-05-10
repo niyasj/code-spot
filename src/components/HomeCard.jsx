5e7b63112b6932271980c1a8f8537943447fa82f
niyasj <93981561+niyasj@users.noreply.github.com>
2023-05-10 23:56:35 +0530

    Update HomeCard.jsx

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -12,25 +12,25 @@ export const HomeCard = (props) =>{
         <h1 class="text-center mb-5">we give Digital solutions for </h1>
            <div class="row text-center">
                
-               <div class="col col-xxl-3 col-xl-3 col-lg-6 col-md-6 col-sm-12 ">
+               <div class="col col-xxl-3 col-xl-3 col-lg-6 col-md-12 col-sm-12 ">
                <img class="cardimg" src={company} alt="company" />
                 <h3>Large Companies</h3>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
                   dummy text ever since the 1500s</p>
                </div>
-              <div class="col col-xxl-3 col-xl-3 col-lg-6 col-md-6 col-sm-12">
+              <div class="col col-xxl-3 col-xl-3 col-lg-6 col-md-12 col-sm-12">
                <img class="cardimg" src={Startups} alt="company" />
                 <h3>Startups</h3>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
                   dummy text ever since the 1500s</p>
                </div>
-               <div class="col col-xxl-3 col-xl-3 col-lg-6 col-md-6 col-sm-12">
+               <div class="col col-xxl-3 col-xl-3 col-lg-6 col-md-12 col-sm-12">
                <img class="cardimg" src={local} alt="company" />
                 <h3>Local Business</h3>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
                   dummy text ever since the 1500s</p>
                </div>
-               <div class="col col-xxl-3 col-xl-3 col-lg-6 col-md-6 col-sm-12">
+               <div class="col col-xxl-3 col-xl-3 col-lg-6 col-md-12 col-sm-12">
                <img class="cardimg" src={Blog} alt="company" />
                 <h3>Blog</h3>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
